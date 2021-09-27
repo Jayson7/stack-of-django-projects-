e199bbc74450a50218ae912088ab57d716c6206e
jayson7 <lexxiijoo70gmail.com>
2021-09-27 03:22:48 +0100

    ecommerce

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,10 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from apps import views
+from apps import views 
 from apps.views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.homepage, name="home"),
-
+    path('admin/', admin.site.urls),
 ]
